6ebb81f39d85bf1f2720601c7008ce9a55aa3193
darlonsantos <darlon49@hotmail.com>
2018-02-25 13:55:55 -0300

    darlon santos

--- FNTS_Comuns PDV/senha.pas
+++ FNTS_Comuns PDV/senha.pas
@@ -6,25 +6,32 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, Mask, RzEdit, AdvOfficeImage, ExtCtrls,
   AdvReflectionLabel, RzLabel, RzBckgnd, pngimage, AdvReflectionImage, jpeg,
-  acPNG, dxGDIPlusClasses;
+  acPNG, Vcl.Buttons, dxGDIPlusClasses;
 
 type
   Tfrmsenha = class(TForm)
+    Image1: TImage;
+    img1: TImage;
+    Label2: TLabel;
+    ed_senha: TRzEdit;
     Label1: TLabel;
     ed_codigo: TRzEdit;
-    ed_senha: TRzEdit;
-    Label2: TLabel;
-    lbl1: TLabel;
+    Image2: TImage;
+    btn_entrar: TSpeedButton;
+    btn_cancelar: TSpeedButton;
+    pnl1: TPanel;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure ed_senhaKeyPress(Sender: TObject; var Key: Char);
     procedure ed_codigoKeyPress(Sender: TObject; var Key: Char);
     procedure FormShow(Sender: TObject);
+    procedure btn_cancelarClick(Sender: TObject);
+    procedure btn_entrarClick(Sender: TObject);
   private
     { Private declarations }
-    function GeraMD5PAFECF: string;
+    function GeraMD5PAFECF: String;
   public
     { Public declarations }
-    function LogarAutomaticamente: Boolean;
+    Function LogarAutomaticamente: Boolean;
   end;
 
 var
@@ -32,13 +39,14 @@ var
 
 implementation
 
-uses funcoes, modulo, principal, md52, IniFiles, UFuncoes; // unit de leitura do arquivo INI;
+uses funcoes, modulo, principal, md52, IniFiles, UFuncoes;
+// unit de leitura do arquivo INI;
 
 {$R *.dfm}
 
 procedure Tfrmsenha.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-  action := cafree;
+  Action := cafree;
 end;
 
 procedure Tfrmsenha.ed_senhaKeyPress(Sender: TObject; var Key: Char);
@@ -73,7 +81,25 @@ begin
     end;
   end;
 end;
-
+procedure Tfrmsenha.btn_cancelarClick(Sender: TObject);
+begin
+frmsenha.close;
+end;
+procedure Tfrmsenha.btn_entrarClick(Sender: TObject);
+begin
+   if not Executa_Login(ed_codigo.Text, ed_senha.Text, sfuncao_senha) then
+    begin
+      application.messagebox('Acesso não autorizado!', 'Erro',
+        mb_ok + mb_iconerror);
+      busuario_Autenticado := false;
+      exit;
+    end
+    else
+      begin
+       busuario_Autenticado := true;
+        close;
+      end;
+  end;
 procedure Tfrmsenha.ed_codigoKeyPress(Sender: TObject; var Key: Char);
 begin
   if key = #13 then
@@ -104,19 +130,21 @@ end;
 procedure Tfrmsenha.FormShow(Sender: TObject);
 begin
   Brush.Style := bsClear;
-//  GeraMD5PAFECF;
+  ed_codigo.SetFocus;
+  // GeraMD5PAFECF;
 end;
 
-function Tfrmsenha.GeraMD5PAFECF: string;
-var Arquivo: TextFile;
-  NomeArq, ArqTexto, aMD5: string;
+function Tfrmsenha.GeraMD5PAFECF: String;
+var
+  Arquivo: TextFile;
+  NomeArq, ArqTexto, aMD5: String;
   F: TSearchRec;
   Ret: Integer;
   Arquivo_ini: TIniFile; // nome do arquivo .ini
 begin
   try
     // Apaga arquivo anterior
-    ArqTexto := ExtractFilePath(Application.ExeName) + 'ArqMD5.txt';
+    ArqTexto := ExtractFilePath(application.ExeName) + 'ArqMD5.txt';
     if FileExists(ArqTexto) then
       DeleteFile(ArqTexto);
     // cria o arquivo TXT na mesma pasta do sistema
@@ -125,24 +153,26 @@ begin
     // lista todos os arquivo exe da pasta do aplicativo e gera o MF5
     try
       // executáveis
-      Ret := FindFirst(ExtractFilePath(Application.ExeName) + '\*.exe', faAnyFile, F);
+      Ret := FindFirst(ExtractFilePath(application.ExeName) + '\*.exe',
+        faAnyFile, F);
       while Ret = 0 do
       begin
         if F.Attr and faDirectory <> faDirectory then
         begin
-          NomeArq := ExtractFilePath(Application.ExeName) + F.Name;
+          NomeArq := ExtractFilePath(application.ExeName) + F.Name;
           aMD5 := MD5Print(MD5File(NomeArq));
           WriteLn(Arquivo, NomeArq + ' - ' + aMD5);
         end;
         Ret := FindNext(F);
       end;
       // DLL's
-      Ret := FindFirst(ExtractFilePath(Application.ExeName) + '\cfg\*.dll', faAnyFile, F);
+      Ret := FindFirst(ExtractFilePath(application.ExeName) + '\cfg\*.dll',
+        faAnyFile, F);
       while Ret = 0 do
       begin
         if F.Attr and faDirectory <> faDirectory then
         begin
-          NomeArq := ExtractFilePath(Application.ExeName) + F.Name;
+          NomeArq := ExtractFilePath(application.ExeName) + F.Name;
           aMD5 := MD5Print(MD5File(NomeArq));
           WriteLn(Arquivo, NomeArq + ' - ' + aMD5);
         end;
@@ -156,9 +186,9 @@ begin
   finally
     Result := MD5Print(MD5File(ArqTexto));
   end;
-    // abrir arquivo .ini
+  // abrir arquivo .ini
   Arquivo_ini := TIniFile.Create('.\cfg\Paf.ini');
-    //MD5
+  // MD5
   Arquivo_ini.WriteString('D37DEB63', '2329383C', Cript('C', Result));
 
 end;
@@ -188,4 +218,3 @@ begin
 end;
 
 end.
-
